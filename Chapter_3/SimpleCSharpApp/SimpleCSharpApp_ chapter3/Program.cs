52d8a5f9d09f834f21f3250f5542936f3abf8c2d
Иван Тетерин <i.teterin@eot-agregator.ru>
2022-02-06 06:52:51 +0300

    get stacktrace and OS environment

--- Chapter_3/SimpleCSharpApp/SimpleCSharpApp_ chapter3/Program.cs
+++ Chapter_3/SimpleCSharpApp/SimpleCSharpApp_ chapter3/Program.cs
@@ -14,7 +14,7 @@ namespace SimpleCSharpApp_chapter3
             Console.WriteLine("***** My First C# App *****");
             Console.WriteLine("Hello World");
             Console.WriteLine();
-            
+
             // Вывести на экран аргументы при запуске программы
             // for (int i = 0; i < args.Length; i++)
             //     Console.WriteLine("Arg: {0}", args[i]);
@@ -24,10 +24,12 @@ namespace SimpleCSharpApp_chapter3
             //     Console.WriteLine("Arg: {0}", arg);
 
             // Получение аргументов запуска программы с помощью класса Envirinment
-            String[] theArgs = Environment.GetCommandLineArgs();
-            foreach(string arg in theArgs)
-                Console.WriteLine("Arg: {0}", arg);
+            // String[] theArgs = Environment.GetCommandLineArgs();
+            // foreach(string arg in theArgs)
+            //     Console.WriteLine("Arg: {0}", arg);
 
+            // Вспомогательный метод внутри класса Program
+            ShowEnvironmentDetails();
 
             // Ожидать нажатия клавиши <Enter>, прежде чем завершить работу.
             Console.ReadLine();
@@ -35,5 +37,17 @@ namespace SimpleCSharpApp_chapter3
             // Возвратить код ошибки
             return -1;
         }
+
+        static void ShowEnvironmentDetails()
+        {
+            // Вывести информацию у дисковых устройствах
+            // данной машины и другие интересные делали
+            foreach (string drive in Environment.GetLogicalDrives())
+                Console.WriteLine("Drive: {0}", drive);
+            Console.WriteLine("OS: {0}", Environment.OSVersion);
+            Console.WriteLine("StackTrace: {0}", Environment.StackTrace);
+            Console.WriteLine(".Net version: {0}", Environment.Version);
+        }
+
     }
 }
